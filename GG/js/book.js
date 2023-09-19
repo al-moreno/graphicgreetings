8cc2d7105a7a5ab9defd96415190a659504bce39
Ana Moreno <ana.moreno@codeup.com>
2023-09-19 17:16:12 -0500

    got everything in place.

--- GG/js/book.js
+++ GG/js/book.js
@@ -23,3 +23,28 @@ document.getElementById("contact-form").addEventListener("submit", function(e) {
      if (like) like.insertAdjacentHTML("beforebegin",`<tr><td colspan="2">Things you like:</td></tr>`)
   });
   
+
+
+  // Booking form submistion
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault(); // stop submission
+  const html = [...this.elements]
+    .filter(ele => {
+      if (ele.type === "text" || ele.type==="email") return true;
+      if (ele.type === "checkbox" && ele.checked) return true;
+      if (ele.type === "submit" || ele.type === "reset") return false;
+    })
+    .map(ele => {
+      const name = ele.name
+      const value = ele.value;
+      if (ele.classList.contains("thingslike")) return `<tr class="thingslike"><td>${ele.name}</td><td>&nbsp;</td></tr>`
+      else return `<tr><td>${ele.name}</td><td>${ele.value}</td></tr>`
+    }).join("");
+  document.getElementById("res-2").innerHTML = 
+  `<h2 class="h1-responsive font-weight-bold text-center my-5">THANK YOU FOR YOUR SUBMISSION!</h2> 
+  <p class="text-center w-responsive mx-auto mb-5">
+  A team member will be in touch within 24 hours.</p>`;
+   const like = document.querySelector("#answer .thingslike");
+   if (like) like.insertAdjacentHTML("beforebegin",`<tr><td colspan="2">Things you like:</td></tr>`)
+});
+
